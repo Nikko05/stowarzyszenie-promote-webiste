13a615bc3c2a9f6df3b00ad73ba3a70a1c52ddb1
niko <nikodemkusiak59@gmail.com>
2022-06-21 15:35:43 +0200

    Add initial files

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const roman = 100;
+
+const animals = ["Józef", "Adam", "Niko"];
+
+console.log(`${animals[1]}`);
+
+const name = ["pies", "kot", "człowiek"];
+
+console.log(`${animals[0]} to ${name[0]}.`);
+console.log(`${animals[1]} to ${name[1]}.`);
+console.log(`${animals[2]} to ${name[2]}.`);
